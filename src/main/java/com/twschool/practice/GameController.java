fff247494553ef42b2da45166d7906248a443520
yu shuang <763104106@qq.com>
2020-06-17 21:11:44 +0800

    add GameController

--- src/main/java/com/twschool/practice/GameController.java
+++ src/main/java/com/twschool/practice/GameController.java
@@ -1,9 +1,14 @@
 package com.twschool.practice;
 
 import com.twschool.practice.domain.GameStatus;
+import com.twschool.practice.domain.GuessNumberGame;
+import com.twschool.practice.domain.RandomAnswerGenerator;
+import com.twschool.practice.domain.UserInfo;
 import com.twschool.practice.service.GameService;
+import com.twschool.practice.service.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,21 +26,37 @@ import java.util.Map;
 public class GameController {
 
     @Autowired
+    private UserRepository userRepository;
+
+
     private GameService gameService;
 
-    @GetMapping("/game")
-    public Map<String,String> guess(@RequestParam String guess) {
-        Map<String,String> map = new HashMap<>();
+    @GetMapping("/game/{userId}")
+    public Map<String, String> guess(@PathVariable String userId, @RequestParam String guess) {
+        UserInfo userInfo = userRepository.getUserInfoById(userId);
+        if (userInfo == null) {
+            GuessNumberGame guessNumberGame = new GuessNumberGame(new RandomAnswerGenerator());
+            userInfo = new UserInfo(userId, guessNumberGame, 0, 0);
+            userRepository.setUserInfo(userId, userInfo);
+        } else {
+            if (userInfo.getGuessNumberGame().getStatus() != GameStatus.CONTINUED) {
+                userInfo.setGuessNumberGame(new GuessNumberGame(new RandomAnswerGenerator()));
+            }
+        }
+        gameService = new GameService(userInfo);
 
-        map.put("input",guess);
-        map.put("result",gameService.guess(guess));
+        Map<String, String> map = new HashMap<>();
+
+        map.put("input", guess);
+        map.put("output", gameService.guess(guess));
+        map.put("score", gameService.countScore() + "");
         return map;
     }
 
     @GetMapping("/history")
-    public Map<String,String> history() {
-        Map<String,String> map = new HashMap<>();
-        map.put("result","Input:1 2 3 4    Output:4A0B");
+    public Map<String, String> history() {
+        Map<String, String> map = new HashMap<>();
+        map.put("result", "Input:1 2 3 4    Output:4A0B");
         return map;
     }
 }
