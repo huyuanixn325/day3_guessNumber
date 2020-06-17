3b839fd702bc7eaf7575490ec0fe0403535706b1
yu shuang <763104106@qq.com>
2020-06-17 21:10:33 +0800

    add GameService

--- src/main/java/com/twschool/practice/service/GameService.java
+++ src/main/java/com/twschool/practice/service/GameService.java
@@ -1,6 +1,9 @@
 package com.twschool.practice.service;
 
+import com.twschool.practice.domain.Answer;
+import com.twschool.practice.domain.GameStatus;
 import com.twschool.practice.domain.GuessNumberGame;
+import com.twschool.practice.domain.UserInfo;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,15 +18,39 @@ import java.util.List;
 public class GameService {
 
     private GuessNumberGame guessNumberGame;
+    private UserInfo userInfo;
 
-   public GameService(GuessNumberGame guessNumberGame) {
-       this.guessNumberGame = guessNumberGame;
-   }
+    public GameService(UserInfo userInfo) {
+        this.userInfo = userInfo;
+        this.guessNumberGame = userInfo.getGuessNumberGame();
+    }
 
-   public String guess(String userAnswerString) {
-       List<String> userAnswer = Arrays.asList(userAnswerString.split(" "));
-       return guessNumberGame.guess(userAnswer);
-   }
+
+    public String guess(String userAnswerString) {
+        List<String> userAnswerNumbers = Arrays.asList(userAnswerString.split(" "));
+        Answer userAnswer = new Answer(userAnswerNumbers);
+        if (!userAnswer.isValidFormat()) {
+            return "Wrong Input，Input again";
+        }
+        return guessNumberGame.guess(userAnswerNumbers);
+    }
+
+    public int countScore() {
+        if (guessNumberGame.getStatus() == GameStatus.FAILED) {
+           userInfo.addScore(-3);
+           userInfo.initWinCount();
+
+        } else if (guessNumberGame.getStatus() == GameStatus.SUCCEED) {
+            userInfo.addScore(3);
+            userInfo.addWinCount();
+            if(userInfo.getWinCount()%5==0){
+                userInfo.addScore(3);
+            }else if(userInfo.getWinCount()%3==0){
+                userInfo.addScore(2);
+            }
+        }
+        return userInfo.getScore();
+    }
 
     public GuessNumberGame getGuessNumberGame() {
         return guessNumberGame;
